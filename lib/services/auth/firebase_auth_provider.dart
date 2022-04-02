6943ea8e121d4338a06834fbaff3e13e59a32c5c
serkanozell <81927386+serkanozell@users.noreply.github.com>
2022-04-02 16:17:17 +0300

    step-24

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -3,7 +3,6 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
-
 import 'package:firebase_auth/firebase_auth.dart'
     show FirebaseAuth, FirebaseAuthException;
 
